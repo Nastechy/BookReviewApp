38b70855ac9b364a286fe059e1975d6396f63a4a
Nastechy <Ibinichinasajoy@gmail.com>
2024-11-13 11:20:03 +0100

    modifications

--- src/FilePages/homePage.tsx
+++ src/FilePages/homePage.tsx
@@ -9,7 +9,7 @@ interface BookListProps {
 
 const HomePage: React.FC<BookListProps> = ({ books }) => {
     return (
-        <div className='p-8' style={{border: '2px solid red'}}>
+        <div className='p-8'>
             <h1 className="text-3xl font-bold mb-6">Featured Books</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
                 {books.map((book) => (
